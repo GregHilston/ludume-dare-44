4584f871f132b10fd5278dc445eb655b59ec3cd9
Derek Sharp <fulminstudios@gmail.com>
2019-04-27 11:31:41 -0600

    Object Pooling and stuff!

---
+++ Ludum Dare 44/Assets/_Scripts/ObjectPool.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+
+    [SerializeField]
+    private List<ObjectPoolItem> poolList = new List<ObjectPoolItem>();
+    [SerializeField]
+    private int maxItemsInPool = 10;
+    [SerializeField]
+
+    public void AddItemToPool(ObjectPoolItem poolItem) {
+        if (poolList.Count < maxItemsInPool) {
+            poolList.Add(poolItem);
+        }
+    }
+
+    public void ClearPool() {
+        poolList.Clear();
+    }
+
+    public bool AtCapacity() {
+        return poolList.Count == maxItemsInPool;
+    }
+
+    public GameObject GetObjectFromPool(GameObject type, bool mustBeState = false, bool state = false) {
+        return GetObjectFromPool(type, Vector3.zero,Quaternion.identity,mustBeState,state);
+    }
+
+    public GameObject GetObjectFromPool(GameObject type, Vector3 position, bool mustBeState = false, bool state = false) {
+        return GetObjectFromPool(type, position,Quaternion.identity,mustBeState,state);
+    }
+
+    public GameObject GetObjectFromPool(GameObject type, Quaternion rotation, bool mustBeState = false, bool state = false) {
+        return GetObjectFromPool(type, Vector3.zero,rotation,mustBeState,state);
+    }
+
+    public GameObject GetObjectFromPool(GameObject type, Vector3 position, Quaternion rotation, bool mustBeState = false, bool state = false) {
+        GameObject returnItem = null;
+        if (!AtCapacity()) {
+            returnItem = Instantiate(type,position,rotation,transform);
+            AddItemToPool(new ObjectPoolItem(returnItem,type));
+        } else {
+            foreach(ObjectPoolItem poolItem in poolList) {
+                if (poolItem.itemSource == type) {
+                    if (mustBeState) {
+                        if (poolItem.itemInstance.activeInHierarchy == state) {
+                            returnItem = poolItem.itemInstance;
+                        }
+                    } else {
+                        returnItem = poolItem.itemInstance;
+                    }
+                }
+            }
+        }
+        if (returnItem != null) {
+            returnItem.transform.position = position;
+            returnItem.transform.rotation = rotation;
+        }
+        return returnItem;
+    }
+
+}
+
+[System.Serializable]
+public class ObjectPoolItem {
+    public GameObject itemInstance {get;}
+    public GameObject itemSource {get;}
+
+    public ObjectPoolItem(GameObject instance, GameObject source) {
+        itemInstance = instance;
+        itemSource = source;
+    }
+}
---
+++ Ludum Dare 44/Assets/_Scripts/PlayerAttack_SingleThrow.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack_SingleThrow : MonoBehaviour {
+
+    [SerializeField]
+    private float cooldownTime;
+    private float cooldownTimer = 0;
+
+    [SerializeField]
+    private GameObject coinPrefab;
+    [SerializeField]
+    private ObjectPool coinPool;
+
+    void Start() {
+
+    }
+
+    void Initialize() {
+
+    }
+
+    void Update() {
+        Attack();
+    }
+
+    void Attack() {
+        if (attackReady()) {
+            DoAttack();
+        }
+    }
+
+    /// <summary>
+    /// Decrements the timer if its value is greater than 0. Returns true if its value is 0, false if higher. Call this on every frame for accurate timer functionality.
+    /// </summary>
+    bool attackReady() {
+        if (cooldownTimer > 0) {
+            cooldownTimer -= Time.deltaTime;
+            cooldownTimer = Mathf.Clamp(cooldownTimer,0,cooldownTimer);
+        }
+        return cooldownTimer <= 0;
+    }
+
+    void ResetTimer() {
+        cooldownTimer = cooldownTime;
+    }
+
+    void DoAttack() {
+        // Using KeyCode.E for testing purposes. Will replace this for better input button later.
+        if (Input.GetKeyDown(KeyCode.E)) {
+            coinPool.GetObjectFromPool(coinPrefab,transform.position,Quaternion.identity);
+            ResetTimer();
+        }
+    }
+
+}
