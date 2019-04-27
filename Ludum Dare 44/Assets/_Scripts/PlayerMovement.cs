4471a72f6a2ef9d7e567df022a12893599507c48
Derek Sharp <fulminstudios@gmail.com>
2019-04-27 00:11:01 -0600

    Fixed Shiz

--- Ludum Dare 44/Assets/_Scripts/PlayerMovement.cs
+++ Ludum Dare 44/Assets/_Scripts/PlayerMovement.cs
@@ -1,54 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody rb;
-    public enum Playertypes{mainplayer,drone};
-    public Playertypes playerType;
-    public float moveSpeed = 10f;
-    private RigidbodyConstraints rbConstraints;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        rbConstraints = rb.constraints;
+    private enum Playertypes{mainplayer,drone};
 
+    [SerializeField]
+    private Playertypes playerType;
+    [SerializeField]
+    private float moveSpeed = 10f;
+
+    private RigidbodyConstraints rbConstraints;
+    private Dictionary<Playertypes,string[]> playerInputAxes = new Dictionary<Playertypes,string[]>();
+
+    void Start() {
+        Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Initialize() {
+        rb = GetComponent<Rigidbody>();
+        if (rb != null) {
+            rbConstraints = rb.constraints;
+        }
+        playerInputAxes[Playertypes.mainplayer] = new string[2] {"Horizontal", "Vertical"};
+        playerInputAxes[Playertypes.drone] = new string[2] {"Horizontal2", "Vertical2"};
+    }
+
+    void Update() {
         DoMovement();
     }
 
     void DoMovement() {
-        switch(playerType) {
-            case Playertypes.mainplayer:
-                Movement("Horizontal","Vertical");
-            break;
-            case Playertypes.drone:
-                Movement("Horizontal2","Vertical2");
-            break;
-        }
+        Movement(playerInputAxes[playerType][0],playerInputAxes[playerType][1]);
     }
 
     void Movement(string hAxis, string vAxis) {
-        float moveX = Input.GetAxis(hAxis);
-        float moveZ = Input.GetAxis(vAxis);
-        Vector3 moveDir = new Vector3(moveX,0,moveZ).normalized;
-        
-        if (moveDir != Vector3.zero) {
-            Debug.Log("THERE IS INPUT");
-            rb.MovePosition(transform.position + moveDir * moveSpeed * Time.deltaTime);
-            rb.MoveRotation(Quaternion.Euler(new Vector3( 0, Mathf.Atan2( moveX, moveZ) * 180 / Mathf.PI, 0 )));
-            rb.constraints = rbConstraints;
+        if (rb != null) {
+            float moveX = Input.GetAxis(hAxis);
+            float moveZ = Input.GetAxis(vAxis);
+            Vector3 moveDir = new Vector3(moveX,0,moveZ).normalized;
+            
+            if (moveDir != Vector3.zero) {
+                rb.MovePosition(transform.position + moveDir * moveSpeed * Time.deltaTime);
+                rb.MoveRotation(Quaternion.Euler(rotationFromInput(moveX,moveZ)));
+                rb.constraints = rbConstraints;
+            } else {
+                rb.velocity = Vector3.up * rb.velocity.y;
+                rb.constraints = RigidbodyConstraints.FreezeRotation;
+            }  
         } else {
-            Debug.Log("NO INPUT");
-            rb.velocity = Vector3.up * rb.velocity.y;
-            rb.constraints = RigidbodyConstraints.FreezeRotation;
+            Debug.LogError(gameObject.name + " does not have a Rigidbody enabled.");
         }
+    }
+
+    Vector3 rotationFromInput(float moveX, float moveZ) {
+        return new Vector3( 0, Mathf.Atan2( moveX, moveZ) * 180 / Mathf.PI, 0 );
     }
 }
