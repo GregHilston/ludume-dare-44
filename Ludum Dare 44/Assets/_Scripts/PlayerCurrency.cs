fd5b08600fb67108e8e5f0fbad26f015aa0e75fd
Derek Sharp <fulminstudios@gmail.com>
2019-04-27 01:06:21 -0600

    Laying the groundwork for combat and UI

---
+++ Ludum Dare 44/Assets/_Scripts/PlayerCurrency.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class UnityObjEvent : UnityEvent<object> {
+
+}
+
+public class PlayerCurrency : MonoBehaviour {
+    
+    public UnityObjEvent OnCurrencyAmountChange;
+
+    private int currencyAmount;
+    [SerializeField]
+    private int startingCurrency = 100;
+
+    void Start() {
+        ChangeCurrency(startingCurrency);
+    }
+
+    public int GetCurrency() {
+        return currencyAmount;
+    }
+
+    public void ChangeCurrency(int amount) {
+        currencyAmount += amount;
+        currencyAmount = Mathf.Clamp(currencyAmount,0,currencyAmount);
+        OnCurrencyAmountChange.Invoke(currencyAmount);
+    }
+
+
+}
