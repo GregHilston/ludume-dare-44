2e0a36ea23346896a16cf6883f713f15eecee26f
Derek Sharp <fulminstudios@gmail.com>
2019-04-28 10:51:42 -0600

    Enemy Navmesh setup

--- Ludum Dare 44/Assets/_Scripts/EnemyObject.cs
+++ Ludum Dare 44/Assets/_Scripts/EnemyObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy", order = 51)]
 public class EnemyObject : ScriptableObject {
